ee96f40bc39f5285fee97e5029b35c4ce20bf5b9
Rémi Weng <ixartz@users.noreply.github.com>
2014-12-18 05:47:36 +0100

    Avoid the influence between two neighbor pixel

--- src/simulated-annealing.cpp
+++ src/simulated-annealing.cpp
@@ -34,20 +34,24 @@ void simulated_annealing(cv::Mat& img)
         delta_global_enery = 0.;
         c.mean_std(output, prob);
 
-        for (int i = 0; i < prob.rows; ++i)
+        // k avoids the influence by neighborhood
+        for (int k = 0; k < 2; ++k)
         {
-            for (int j = 0; j < prob.cols; ++j)
+            for (int i = 0; i < prob.rows; ++i)
             {
-                new_classe = uint_dist(eng) % 5;
-                delta = c.compute(output, i, j, new_classe) -
-                        c.compute(output, i, j, prob.at<uchar>(i, j));
-
-                if (delta <= 0. ||
-                    exp(-delta / temperature) >= ureal_dist(eng))
+                for (int j = k; j < prob.cols; j += 2)
                 {
-                    delta_global_enery += fabs(delta);
-                    prob.at<uchar>(i, j) = new_classe;
-                    convert(prob, output);
+                    new_classe = uint_dist(eng) % 5;
+                    delta = c.compute(output, i, j, new_classe) -
+                    c.compute(output, i, j, prob.at<uchar>(i, j));
+
+                    if (delta <= 0. ||
+                        exp(-delta / temperature) >= ureal_dist(eng))
+                    {
+                        delta_global_enery += fabs(delta);
+                        prob.at<uchar>(i, j) = new_classe;
+                        convert(prob, output);
+                    }
                 }
             }
         }
